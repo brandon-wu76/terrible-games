de2d82762ba8ff24672903e764c3c3bab0de02b4
Emily Bao <emily.yf.bao@gmail.com>
2020-08-07 18:27:21 -0600

    App.py and button.py change commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame
 import game_config as gc
+from button import Button
 
 from pygame import display, event
 
@@ -9,8 +10,9 @@ window = display.set_mode((gc.DISPLAY_WIDTH, gc.DISPLAY_HEIGHT))
 display.set_caption("Terrible Games")
 pygame.font.init()
 
-titlefont = pygame.font.SysFont(('Comic Sans MS'), 30)
-
+title_font = pygame.font.SysFont(('Comic Sans MS'), 60)
+button_font = pygame.font.SysFont(('Comic Sans MS'), 30)
+tiles_button = Button("Tiles", button_font)
 running = True
 
 while running:
@@ -20,7 +22,16 @@ while running:
 
         if e.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-    titletext = titlefont.render("Terrible Games", False, (0,0,0))
+
     window.fill((255, 255, 255))
-    window.blit(titletext, (0, 0))
+
+    title_text = title_font.render("Terrible Games", True, (0,0,0))
+    title_height = title_text.get_height()
+    title_width = title_text.get_width()
+    window.blit(title_text, ((gc.DISPLAY_WIDTH-title_width)//2,
+                             (gc.DISPLAY_HEIGHT-title_height)//3))
+
+    tiles_button = Button("Tiles", button_font)
+    tiles_button.renderButton(window)
+
     display.flip()
--- button.py
+++ button.py
@@ -1,5 +1,17 @@
 import os
 
 class Button:
-    def __init__(self, button_name):
+    def __init__(self, button_name, button_font):
         self.name = button_name
+        self.surf = button_font.render(self.name, True, (0,0,0))
+        self.xpos = 0
+        self.ypos = 0
+
+    def set_x_pos(self, x):
+        self.xpos = x
+
+    def set_y_pos(self, y):
+        self.ypos = y
+
+    def renderButton(self, window):
+        window.blit(self.surf,(self.xpos,self.ypos))
